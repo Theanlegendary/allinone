767b5152c1db0d725eba3659c9cc984786207728
Theanlegendary <Theanlegendary@users.noreply.github.com>
2026-08-19 09:23:54 +0000

    deploy: ec01e5e32173dcb2d8af45056d4791dec57f7143

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1433010282" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "290084754" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
